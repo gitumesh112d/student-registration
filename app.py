12fa01d909a5b8c4db4133a71731482360bb91cc
umesh gupta <umesh.gupta112ec@gmail.com>
2026-05-23 13:23:09 +0530

    db fetch record fix

--- app.py
+++ app.py
@@ -138,6 +138,6 @@ def send_sms(mobile, fname):
     return response.json()
 
 if __name__ == "__main__":
-      app.run(debug=False, host='0.0.0.0', port=int(os.environ.get("PORT", 3000)))
+      app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 3000)))
 
 
